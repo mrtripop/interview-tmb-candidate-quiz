1a7bb913530f6bfc8243a335748125dd0ce682c8
mrtripop <tripop.chai12@gmail.com>
2024-06-25 23:09:47 +0700

    test: find number nine logic

---
+++ src/test/java/com/tmb/CandidateQuiz/components/FindNumberTest.java
@@ -0,0 +1,31 @@
+package com.tmb.CandidateQuiz.components;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class FindNumberTest {
+
+  List<Integer> listNumber;
+
+  private FindNumber findNumber;
+
+  @BeforeEach
+  void setup() {
+    listNumber = Arrays.asList(18, 0, -3, 66, 28, 9, -70, 120);
+    findNumber = new FindNumber(listNumber);
+  }
+
+  @Test
+  @DisplayName("[SUCCESS] Find number nine in array")
+  void success_find_nine_number_in_array() throws Exception {
+    // action
+    Integer result = findNumber.findNine();
+    // assert
+    assertEquals(9, result);
+  }
+}
