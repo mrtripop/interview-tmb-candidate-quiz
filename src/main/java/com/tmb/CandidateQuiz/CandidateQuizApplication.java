7e0347539fc439132812cf6bb89033eafb20e698
mrtripop <tripop.chai12@gmail.com>
2024-06-25 20:20:04 +0700

    initial project

---
+++ src/main/java/com/tmb/CandidateQuiz/CandidateQuizApplication.java
@@ -0,0 +1,13 @@
+package com.tmb.CandidateQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidateQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmb/CandidateQuiz/CandidateQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmb.CandidateQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
