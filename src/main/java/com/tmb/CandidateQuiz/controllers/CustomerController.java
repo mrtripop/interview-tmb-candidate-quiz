f4eb262935e04cebd2c96ad1f311dd199636a775
mrtripop <tripop.chai12@gmail.com>
2024-06-25 23:10:17 +0700

    feat: implement API for get customer name

---
+++ src/main/java/com/tmb/CandidateQuiz/controllers/CustomerController.java
@@ -0,0 +1,24 @@
+package com.tmb.CandidateQuiz.controllers;
+
+import com.tmb.CandidateQuiz.models.Customer;
+import com.tmb.CandidateQuiz.models.CustomerDTO;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CustomerController {
+
+  @PostMapping("/v1/getCustomer")
+  public HttpEntity<Customer> getCustomerById(@RequestBody CustomerDTO customer) {
+    Customer existingCustomer = new Customer(customer.getCustomerId(), "NAME TEST");
+    return new ResponseEntity<>(existingCustomer, HttpStatus.OK);
+  }
+
+  @GetMapping("/v1/customers/{customerId}")
+  public HttpEntity<Customer> getCustomerById(@PathVariable String customerId) {
+    Customer customer = new Customer(customerId, "NAME TEST");
+    return new ResponseEntity<>(customer, HttpStatus.OK);
+  }
+}
